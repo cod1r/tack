cb1165c4cfd2dfbfa36e15b062b62c77af0c5fa3
cod1r <jasonho353@gmail.com>
2025-03-27 01:28:58 -0500

    more stub code and bindings

--- lib/opengl_stubs.c
+++ lib/opengl_stubs.c
@@ -1,4 +1,5 @@
 #ifdef __APPLE__
+#define GL_SILENCE_DEPRECATION
 #include <OpenGL/gl.h>
 #endif
 #ifdef __LINUX__
@@ -15,12 +16,30 @@
 #include <caml/fail.h>
 #include <stdio.h>
 
-//CAMLprim value load_vertices(value bigarray, value size) {
-//  glGenBuffers();
-//  glBindBuffer();
-//  glBufferData();
-//  glVertexAttribPointer();
-//}
+CAMLprim value gl_gen_buffers(value num) {
+  CAMLparam1(num);
+  GLuint buffer;
+  glGenBuffers(num, &buffer);
+  CAMLreturn(buffer);
+}
+
+CAMLprim value gl_vertex_attrib_pointer_float_type(value location, value size, value normalized) {
+  CAMLparam3(location, size, normalized);
+  glVertexAttribPointer(Int_val(location), Int_val(size), GL_FLOAT, Bool_val(normalized), sizeof(float) * Int_val(size), 0);
+  CAMLreturn(Val_unit);
+}
+
+CAMLprim value gl_draw_arrays(value num_points) {
+  CAMLparam1(num_points);
+  glDrawArrays(GL_POINTS, 0, Int_val(num_points));
+  CAMLreturn(Val_unit);
+}
+
+CAMLprim value gl_bind_buffer(value buffer) {
+  CAMLparam1(buffer);
+  glBindBuffer(GL_ARRAY_BUFFER, Int_val(buffer));
+  CAMLreturn(Val_unit);
+}
 
 CAMLprim value gl_clear_color(double r, double g, double b, double a) {
   CAMLparam0();
@@ -48,14 +67,29 @@ CAMLprim value gl_getattriblocation(value program, value name) {
   CAMLreturn(result);
 }
 
-CAMLprim value gl_createshader(value type) {
-  CAMLparam1(type);
+CAMLprim value gl_create_fragment_shader(value unit) {
+  CAMLparam1(unit);
   CAMLlocal1(result);
-  GLenum shader_type = Int_val(type) == 0 ? GL_VERTEX_SHADER : GL_FRAGMENT_SHADER;
+  GLenum shader_type = GL_FRAGMENT_SHADER;
   GLuint shader = glCreateShader(shader_type);
   if (shader == 0) {
     result = caml_alloc(1, 1);
-    Store_field(result, 0, caml_copy_string("shader couldn't be created"));
+    Store_field(result, 0, caml_copy_string("vertex shader couldn't be created"));
+    CAMLreturn(result);
+  }
+  result = caml_alloc(1, 0);
+  Store_field(result, 0, Val_int(shader));
+  CAMLreturn(result);
+}
+
+CAMLprim value gl_create_vertex_shader(value unit) {
+  CAMLparam1(unit);
+  CAMLlocal1(result);
+  GLenum shader_type = GL_VERTEX_SHADER;
+  GLuint shader = glCreateShader(shader_type);
+  if (shader == 0) {
+    result = caml_alloc(1, 1);
+    Store_field(result, 0, caml_copy_string("vertex shader couldn't be created"));
     CAMLreturn(result);
   }
   result = caml_alloc(1, 0);
