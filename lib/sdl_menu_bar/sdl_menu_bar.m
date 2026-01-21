df9508f38080ef9fb68ac6a75d4599687d1ad2cf
cod1r <jasonho353@gmail.com>
2026-01-21 17:34:39 -0600

    notes for the future in case I dont know wtf is happening with obj c

--- lib/sdl_menu_bar/sdl_menu_bar.m
+++ lib/sdl_menu_bar/sdl_menu_bar.m
@@ -2,6 +2,16 @@
 #include <AppKit/AppKit.h>
 #include <caml/callback.h>
 
+/*
+according to
+https://developer.apple.com/library/archive/documentation/Cocoa/Conceptual/MenuList/Articles/EnablingMenuItems.html
+
+"If the menu item’s target is set, then NSMenu first checks to see if that object implements the item’s action method. If it does not, then the item is disabled. If the target does implement the item’s action method, NSMenu first checks to see if that object implements validateMenuItem: or validateUserInterfaceItem: method. If it does not, then the menu item is enabled. If it does, then the enabled status of the menu item is determined by the return value of the method."
+
+So I just make an interface/class that extends or inherits from NSMenuItem
+and then implement the action function in that interface/class which
+allows the menuitem to be enabled.
+*/
 @interface SaveMenuItem : NSMenuItem
     +(SaveMenuItem*)get_menu_item;
     +(void) save_function;
